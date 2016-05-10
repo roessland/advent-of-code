638c65dbdc4d38adc41eaf665b14756bf553f7c6
Andreas Røssland <andreas.roessland@gmail.com>
2016-05-10 09:37:56 -0400

    do 19 with a little help

--- day-19/part2.go
+++ day-19/part2.go
@@ -1,14 +1,15 @@
 package main
 
-import "regexp"
-import "fmt"
 import "io/ioutil"
 import "strings"
-import "os"
+import "math/rand"
+import "fmt"
 
-func ReadInput(filename string) (map[string][]string, string) {
+type Rep [2]string
+
+func ReadInput(filename string) ([]Rep, string) {
 	buf, _ := ioutil.ReadFile(filename)
-	rep := make(map[string][]string)
+	reps := []Rep{}
 	var mol string
 	lastLine := false
 	for _, line := range strings.Split(string(buf), "\n") {
@@ -22,48 +23,37 @@ func ReadInput(filename string) (map[string][]string, string) {
 		}
 		fromto := strings.Split(line, " => ")
 		from, to := fromto[0], fromto[1]
-		if _, ok := rep[from]; !ok {
-			rep[from] = []string{}
-		}
-		rep[from] = append(rep[from], to)
+		reps = append(reps, Rep{from, to})
 	}
-	return rep, mol
-}
-
-func PossibleResults(mol0 string, rep map[string][]string) map[string]bool {
-	possibleMols := make(map[string]bool)
-	var atoms0 []string
-	if mol0 == "e" {
-		atoms0 = []string{"e"}
-	} else {
-		atoms0 = regexp.MustCompile(`[A-Z][a-z]*`).FindAllString(mol0, -1)
-	}
-	for i, atom0 := range atoms0 {
-		originalAtom0 := atom0
-		for _, to := range rep[originalAtom0] {
-			atoms0[i] = to
-			possibleMols[strings.Join(atoms0, "")] = true
-		}
-		atoms0[i] = originalAtom0
-	}
-	return possibleMols
+	return reps, mol
 }
 
 func main() {
-	rep, target := ReadInput("input.txt")
-	steps := make([]map[string]bool, 1)
-	steps[0] = map[string]bool{"e": true}
-	for step := 1; step < 600; step++ {
-		steps = append(steps, map[string]bool{})
-		for mol, _ := range steps[step-1] {
-			for res, _ := range PossibleResults(mol, rep) {
-				steps[step][res] = true
-				if res == target {
-					fmt.Printf("Found it during step %v\n", step)
-					os.Exit(0)
-				}
+	reps, mol0 := ReadInput("input.txt")
+	mol := mol0
+
+	count := 0
+	for mol != "e" {
+		molBefore := mol
+		// Shuffle replacements
+		for i := range reps {
+			j := rand.Intn(i + 1)
+			reps[i], reps[j] = reps[j], reps[i]
+		}
+
+		// Apply all possible replacements
+		for _, rep := range reps {
+			if strings.Index(mol, rep[1]) == -1 {
+				continue
 			}
+			mol = strings.Replace(mol, rep[1], rep[0], 1)
+			count++
+		}
+		if mol == molBefore {
+			// No replacements could be made, restart with original molecule.
+			mol = mol0
+			count = 0
 		}
 	}
-	//fmt.Printf("%v\n", steps)
+	fmt.Printf("Depth: %v\n", count)
 }
