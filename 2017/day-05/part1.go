c46d5d86bc5b0c6c26121e83bf4a6d93f85486f5
Andreas Røssland <andreas.roessland@gmail.com>
2017-12-14 13:10:11 +0000

    day 5 in golang

---
+++ 2017/day-05/part1.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+import "encoding/csv"
+import "log"
+import "os"
+import "strconv"
+
+func ReadInput() []int {
+	reader := csv.NewReader(os.Stdin)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	a := make([]int, len(lines))
+	for i, _ := range lines {
+		num, err := strconv.Atoi(lines[i][0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		a[i] = num
+	}
+	return a
+}
+
+func main() {
+	a := ReadInput()
+	ip := 0
+	steps := 0
+	for {
+		prev := ip
+		ip += a[ip]
+		a[prev]++
+		steps++
+		if ip < 0 || len(a) <= ip {
+			break
+		}
+	}
+	fmt.Println(steps)
+}
