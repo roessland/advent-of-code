07455d44679fd7f26ff50a26022bb4c856189f3f
Andreas Røssland <andreas.roessland@gmail.com>
2017-12-17 17:12:47 +0000

    optimize part 2

--- 2017/day-17/part2.go
+++ 2017/day-17/part2.go
@@ -6,7 +6,11 @@ func main() {
 	step := 335
 	ans := -1
 	for i, pos, size := 1, 0, 1; i <= 50000000; i, size, pos = i+1, size+1, pos+1 {
-		pos = (pos + step) % size
+		if pos+step >= size {
+			pos = (pos + step) % size
+		} else {
+			pos = pos + step
+		}
 		if pos == 0 {
 			ans = i
 		}
