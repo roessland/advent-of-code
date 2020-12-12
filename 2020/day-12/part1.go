1737fd2cb1ef55ed969d21fe531f363792107a1f
Andreas Røssland <andreas.roessland@gmail.com>
2020-12-12 21:14:57 +0100

    2020 day 12 in Go, ship movement and waypoint vector stuff

---
+++ 2020/day-12/part1.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Vec struct {
+	E, N int
+}
+
+func (p Vec) Rot(degrees int) Vec {
+	degrees = degrees % 360
+	for degrees < 0 {
+		degrees += 360
+	}
+	switch degrees {
+	case 0:
+		return p
+	case 90:
+		return Vec{E: -p.N, N: p.E}
+	case 180:
+		return Vec{E: -p.E, N: -p.N}
+	case 270:
+		return Vec{E: p.N, N: -p.E}
+	default:
+		log.Fatalf("rotating degrees %d", degrees)
+		return Vec{}
+	}
+}
+
+func (p Vec) Add(v Vec) Vec {
+	return Vec{p.E + v.E, p.N + v.N}
+}
+
+func (p Vec) Mul(n int) Vec {
+	return Vec{n*p.E, n*p.N}
+}
+
+type Ship struct {
+	Dir Vec
+	Pos Vec
+}
+
+func (s Ship) Do(cmd uint8, n int) Ship {
+	switch cmd {
+	case 'N':
+		s.Pos.N += n
+	case 'S':
+		s.Pos.N -= n
+	case 'E':
+		s.Pos.E += n
+	case 'W':
+		s.Pos.E -= n
+	case 'L':
+		s.Dir = s.Dir.Rot(n)
+	case 'R':
+		s.Dir = s.Dir.Rot(-n)
+	case 'F':
+		s.Pos = s.Pos.Add(s.Dir.Mul(n))
+	default:
+		log.Fatalf("unknown command %c", cmd)
+	}
+	return s
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func (p Vec) Dist() int {
+	return Abs(p.E) + Abs(p.N)
+}
+
+func main () {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ship := Ship{
+		Dir: Vec{E: 1},
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		cmd := line[0]
+		n, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		ship = ship.Do(cmd, n)
+		fmt.Println(ship)
+	}
+	fmt.Println(ship.Pos.Dist())
+}
