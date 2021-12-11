d9d629f77663c5afd772cf66b624dab8a80d0a07
Andreas Røssland <andreas.rossland@vipps.no>
2021-12-11 16:56:34 +0100

    forgot to add day 9

--- 2021/day-09/part12.go
+++ 2021/day-09/part12.go
@@ -3,130 +3,142 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/roessland/gopkg/disjointset"
 	"log"
-	"math/bits"
 	"os"
-	"strings"
+	"sort"
 )
 
-type Display struct {
-	Inputs  []Signals
-	Outputs []Signals
+
+
+type Map struct {
+	Width, Height int
+	heights       []int
+}
+
+type Pos struct {
+	I, J int
+}
+
+func (m Map) At(pos Pos) int {
+	return m.heights[pos.J*m.Width+pos.I]
+}
+
+// Id returns an unique ID for each position.
+// This is needed by the DisjointSet/UnionFind implementation.
+func (m Map) Id(pos Pos) int {
+	return pos.J*m.Width + pos.I
+}
+
+func (m Map) AllPositions() <-chan Pos {
+	ch := make(chan Pos)
+	go func() {
+		for j := 0; j < m.Height; j++ {
+			for i := 0; i < m.Width; i++ {
+				ch <- Pos{i, j}
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func (m Map) AdjacentPositions(pos Pos) []Pos {
+	var legalAdjPositions []Pos
+	for _, adjPos := range []Pos{
+		{pos.I - 1, pos.J}, {pos.I + 1, pos.J},
+		{pos.I, pos.J - 1}, {pos.I, pos.J + 1},
+	} {
+		if adjPos.I < 0 || adjPos.I >= m.Width {
+			continue
+		}
+		if adjPos.J < 0 || adjPos.J >= m.Height {
+			continue
+		}
+		legalAdjPositions = append(legalAdjPositions, adjPos)
+	}
+	return legalAdjPositions
 }
 
 func main() {
-	patterns := ReadInput()
-	part1(patterns)
-	part2(patterns)
+	m := ReadInput()
+	part1(m)
+	part2(m)
 }
 
-func part1(displays []Display) {
-	n := 0
-	for _, display := range displays {
-		for _, signals := range display.Outputs {
-			if signals.Len() == 2 || signals.Len() == 4 || signals.Len() == 3 || signals.Len() == 7  { // 1
-				n++
+func part1(m Map) {
+	totalRisk := 0
+	for pos := range m.AllPositions() {
+		height := m.At(pos)
+		adjPositions := m.AdjacentPositions(pos)
+		lowPoint := true
+		for _, adjPosition := range adjPositions {
+			adjHeight := m.At(adjPosition)
+			if height >= adjHeight {
+				lowPoint = false
+				break
+			}
+		}
+		if lowPoint {
+			risk := 1 + height
+			totalRisk += risk
+		}
+	}
+	fmt.Println("Part 1:", totalRisk)
+}
+
+func part2(m Map) {
+	// Start with every tile being its own component.
+	basins := disjointset.Make(m.Height * m.Width)
+	for pos := range m.AllPositions() {
+		height := m.At(pos)
+		if height == 9 {
+			continue // Don't join walls with anything.
+		}
+		adjPositions := m.AdjacentPositions(pos)
+		for _, adjPos := range adjPositions {
+			adjHeight := m.At(adjPos)
+			if adjHeight != 9 {
+				// Same basin -- connect the components.
+				basins.Union(m.Id(pos), m.Id(adjPos))
 			}
 		}
 	}
-	fmt.Println("Part 1:", n)
-}
 
-type Signals uint8
-
-func SetOf(str string) Signals {
-	var set Signals
-	for _, c := range str {
-		set = set | (1 << (c - 'a'))
-	}
-	return set
-}
-
-func (set Signals) Len() int {
-	return bits.OnesCount8(uint8(set))
-}
-
-func Decode(p map[int]Signals, signalsList []Signals) int {
-	digitForSignals := make(map[Signals]int)
-	for digit, signals := range p {
-		digitForSignals[signals] = digit
-	}
-
-	out := 0
-	for _, signals := range signalsList {
-		out = out*10 + digitForSignals[signals]
-	}
-	return out
-}
-
-func part2(displays []Display) {
-	totalOutput := 0
-	for _, display := range displays {
-		signalsFor := make(map[int]Signals)
-
-		// First pass -- Get 1, 4, 7 since they are uniquely identified by
-		// the number of segments only.
-		for _, signals := range display.Inputs {
-			if signals.Len() == 2 { // 1
-				signalsFor[1] = signals
-			} else if signals.Len() == 4 { // 4
-				signalsFor[4] = signals
-			} else if signals.Len() == 3 { // 7
-				signalsFor[7] = signals
-			} else if signals.Len() == 7 { // 8
-				signalsFor[8] = signals
-			}
+	// Find the basin ID for every tile.
+	basinSizes := make([]int, m.Width*m.Height)
+	for j := 0; j < m.Height; j++ {
+		for i := 0; i < m.Width; i++ {
+			pos := Pos{i, j}
+			basinId := basins.Find(m.Id(pos))
+			basinSizes[basinId]++
 		}
-
-		// Second pass -- Moderately clever logic based on set operations.
-		for _, signals := range display.Inputs {
-			switch signals {
-			case signalsFor[1], signalsFor[4], signalsFor[7], signalsFor[8]:
-				continue
-			}
-			Len := signals.Len()
-			and7 := (signalsFor[7] & signals).Len()
-			or7 := (signalsFor[7] | signals).Len()
-			and4 := (signalsFor[4] & signals).Len()
-
-			if Len == 6 && and7 == 3 && or7 == 6 && and4 == 3 {
-				signalsFor[0] = signals
-			} else if Len == 5 && and7 == 2 && or7 == 6 && and4 == 2 {
-				signalsFor[2] = signals
-			} else if Len == 5 && and7 == 3  {
-				signalsFor[3] = signals
-			} else if Len == 5 && and7 == 2 && or7 == 6 && and4 == 3  {
-				signalsFor[5] = signals
-			} else if Len == 6 && and7 == 2 {
-				signalsFor[6] = signals
-			} else if Len == 6 && and7 == 3 && or7 == 6 && and4 == 4 {
-				signalsFor[9] = signals
-			}
-		}
-
-		// Decode output
-		totalOutput += Decode(signalsFor, display.Outputs)
 	}
-	fmt.Println("Part 2:", totalOutput)
+	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes)))
+	result := basinSizes[0] * basinSizes[1] * basinSizes[2]
+	fmt.Println("Part 2:", result)
 }
 
-func ReadInput() []Display {
+func ReadInput() Map {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(f)
-	var patterns []Display
+	var lines []string
 	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), " | ")
-		pat := Display{}
-		for _, signalsStr := range strings.Split(parts[0], " ") {
-			pat.Inputs = append(pat.Inputs, SetOf(signalsStr))
-		}
-		for _, signalsStr := range strings.Split(parts[1], " ") {
-			pat.Outputs = append(pat.Outputs, SetOf(signalsStr))
-		}
-		patterns = append(patterns, pat)
+		lines = append(lines, scanner.Text())
 	}
-	return patterns
+
+	m := Map{
+		Height:  len(lines),
+		Width:   len(lines[0]),
+		heights: make([]int, len(lines)*len(lines[0])),
+	}
+	for j, line := range lines {
+		for i, c := range line {
+			m.heights[j*m.Width+i] = int(c - '0')
+		}
+	}
+	return m
 }
