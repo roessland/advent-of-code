3fe2940cd56c5a07f380a3fb82a7584e8c0ccb59
Andreas Røssland <andreas.roessland@gmail.com>
2021-12-14 17:43:52 +0100

    day 13, fix

--- 2021/day-13/part12.go
+++ 2021/day-13/part12.go
@@ -5,89 +5,132 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 )
 
+type Dots map[Pos]int
 
+func (dots Dots) Print() {
+	maxX := 0
+	maxY := 0
+	for pos, _ := range dots {
+		if pos.X > maxX {
+			maxX = pos.X
+		}
+		if pos.Y > maxY {
+			maxY = pos.Y
+		}
+	}
+	im := make([][]rune, maxY+1)
+	for y := 0; y <= maxY; y++ {
+		im[y] = make([]rune, maxX+1)
+	}
+	for pos, _ := range dots {
+		im[pos.Y][pos.X] = '█'
+	}
+	for y := 0; y <= maxY; y++ {
+		fmt.Println(string(im[y]))
+	}
+}
+
+type Pos struct {
+	X, Y int
+}
+
+type Fold struct {
+	Axis rune
+	Val  int
+}
 
 func main() {
-	t0 := time.Now()
-	g := ReadInput()
-	part1(g)
-	part2(g)
-	fmt.Println(time.Since(t0))
-
+	dots0, folds := ReadInput()
+	part1(dots0, folds)
+	part2(dots0, folds)
 }
 
-func part1(g Graph) {
-	visited := map[string]int{}
-	paths := search(g, "start", visited, false)
-	fmt.Println("Part 1:", paths)
-}
-
-func part2(g Graph) {
-	visited := map[string]int{}
-	paths := search(g, "start", visited, true)
-	fmt.Println("Part 2:", paths)
-}
-
-func search(g Graph, fromId string, visited map[string]int, token bool) int {
-	if fromId == "end" {
-		return 1
-	}
-	if IsSmall(fromId) {
-		visited[fromId]++
-	}
-	var paths int
-	for _, neighborId := range g[fromId] {
-		if neighborId == "start" {
-			continue
-		}
-		if visited[neighborId] == 0 {
-			// Visit without using token
-			paths += search(g, neighborId, visited, token)
-		} else if IsSmall(neighborId) && token {
-			// Visit using token
-			paths += search(g, neighborId, visited, false)
-
-		}
-	}
-	if IsSmall(fromId) {
-		visited[fromId]--
-	}
-	return paths
-}
-
-type Graph map[string][]string
-
-func (g Graph) AddNode(id string) {
-	if g[id] == nil {
-		g[id] = make([]string, 0)
+func FoldX(srcPos Pos, fold Fold) Pos {
+	if srcPos.X < fold.Val {
+		return srcPos
+	} else {
+		return Pos{2*fold.Val - srcPos.X, srcPos.Y}
 	}
 }
 
-func (g Graph) AddEdge(nodeId1 string, nodeId2 string) {
-	g[nodeId1] = append(g[nodeId1], nodeId2)
-	g[nodeId2] = append(g[nodeId2], nodeId1)
+func FoldY(srcPos Pos, fold Fold) Pos {
+	if srcPos.Y < fold.Val {
+		return srcPos
+	} else {
+		return Pos{srcPos.X, 2*fold.Val - srcPos.Y}
+	}
 }
 
-func IsSmall(nodeId string) bool {
-	return strings.ToLower(nodeId) == nodeId
+func FoldDot(srcPos Pos, fold Fold) Pos {
+	if fold.Axis == 'x' {
+		return FoldX(srcPos, fold)
+	} else {
+		return FoldY(srcPos, fold)
+	}
 }
 
-func ReadInput() Graph {
+func FoldDots(dots0 Dots, fold Fold) Dots {
+	dots := make(Dots)
+	for srcPos, val := range dots0 {
+		var dstPos Pos
+		dstPos = FoldDot(srcPos, fold)
+		dots[dstPos] += val
+	}
+	return dots
+}
+
+func part1(dots Dots, folds []Fold) {
+	dots = FoldDots(dots, folds[0])
+	fmt.Println("Part 1:", len(dots))
+}
+
+func part2(dots Dots, folds []Fold) {
+	for _, fold := range folds {
+		dots = FoldDots(dots, fold)
+	}
+	fmt.Println("Part 2:")
+	dots.Print()
+}
+
+func ReadInput() (Dots, []Fold) {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(f)
-	g := make(Graph)
+	dots := make(Dots)
 	for scanner.Scan() {
-		nodes := strings.Split(scanner.Text(), "-")
-		g.AddNode(nodes[0])
-		g.AddNode(nodes[1])
-		g.AddEdge(nodes[0], nodes[1])
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		parts := strings.Split(line, ",")
+		x, err1 := strconv.Atoi(parts[0])
+		y, err2 := strconv.Atoi(parts[1])
+		if err1 != nil || err2 != nil {
+			log.Fatal("ops")
+		}
+		dots[Pos{x, y}] = 1
 	}
-	return g
+
+	var folds []Fold
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(strings.Split(line, "fold along ")[1], "=")
+		axis := rune(parts[0][0])
+		val, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatal("nah")
+		}
+		folds = append(folds, Fold{
+			Axis: axis,
+			Val:  val,
+		})
+	}
+
+	return dots, folds
 }
