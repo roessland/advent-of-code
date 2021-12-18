557bca83af0cb2d94fda1446ed02368fd4549dd6
Andreas Røssland <andreas.roessland@gmail.com>
2021-12-18 23:10:15 +0100

    wip day 17, catapult thing

--- 2021/day-17/part12.go
+++ 2021/day-17/part12.go
@@ -2,232 +2,123 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"math"
-
 	. "github.com/roessland/gopkg/mathutil"
 )
 
-type Packet struct {
-	Version      int
-	TypeID       int
-	LiteralValue int64
-	SubPackets   []Packet
+type TargetArea struct {
+	MinX, MaxX, MinY, MaxY int
 }
 
-func main() {
-	packet := ReadInput()
-	fmt.Println("Part 1:", VersionSum(packet))
-	fmt.Println("Part 2:", Value(packet))
+func (area TargetArea) Contains(x, y int) bool {
+	return area.ContainsX(x) && area.ContainsY(y)
 }
 
-func VersionSum(packet Packet) int {
-	versionSum := packet.Version
-	for _, subPacket := range packet.SubPackets {
-		versionSum += VersionSum(subPacket)
-	}
-	return versionSum
+func (area TargetArea) ContainsX(x int) bool {
+	return area.MinX <= x && x <= area.MaxX
 }
 
-func Value(packet Packet) int64 {
-	switch packet.TypeID {
-	case 0:
-		var sum int64 = 0
-		for _, subPacket := range packet.SubPackets {
-			sum += Value(subPacket)
+func (area TargetArea) ContainsY(y int) bool {
+	return area.MinY <= y && y <= area.MaxY
+}
+
+func Simulate(dx, dy int, area TargetArea) (maxHeight int) {
+	x, y := 0, 0
+	for {
+		x += dx
+		y += dy
+		maxHeight = MaxInt(maxHeight, y)
+		dx -= SignInt(x)
+		dy -= 1
+		if area.Contains(x, y) {
+			return maxHeight
 		}
-		return sum
-	case 1:
-		var prod int64 = 1
-		for _, subPacket := range packet.SubPackets {
-			prod *= Value(subPacket)
-		}
-		return prod
-	case 2:
-		var min int64 = math.MaxInt64
-		for _, subPacket := range packet.SubPackets {
-			min = MinInt64(min, Value(subPacket))
-		}
-		return min
-	case 3:
-		var max int64 = math.MinInt64
-		for _, subPacket := range packet.SubPackets {
-			max = MaxInt64(max, Value(subPacket))
-		}
-		return max
-	case 4:
-		return packet.LiteralValue
-	case 5:
-		if Value(packet.SubPackets[0]) > Value(packet.SubPackets[1]) {
-			return 1
-		} else {
-			return 0
-		}
-	case 6:
-		if Value(packet.SubPackets[0]) < Value(packet.SubPackets[1]) {
-			return 1
-		} else {
-			return 0
-		}
-	case 7:
-		if Value(packet.SubPackets[0]) == Value(packet.SubPackets[1]) {
-			return 1
-		} else {
+		if y < area.MinY {
 			return 0
 		}
 	}
-	panic(fmt.Sprintf("unknown type id %d", packet.TypeID))
 }
 
-func ReadInput() Packet {
-	asciiHex, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		log.Fatal(err)
+func FindDx(area TargetArea) (dx int) {
+	for dx = 1; dx <= area.MaxX; dx++ {
+		hit := SimulateX(dx, area)
+		if hit {
+			return dx
+		}
 	}
-
-	packet := NewPacketReader(asciiHex).Read()
-
-	return packet
+	return
 }
 
-type Bit uint8
-
-type Nibble uint8
-
-type Bits []Bit
-
-func (bits Bits) AsInt() int {
-	var n int
-	for i := 0; i < len(bits); i++ {
-		n = n<<1 + int(bits[i])
-	}
-	return n
-}
-
-func (bits Bits) AsInt64() int64 {
-	var n int64
-	for i := 0; i < len(bits); i++ {
-		n = n<<1 + int64(bits[i])
-	}
-	return n
-}
-
-var hexMapping = map[byte]Nibble{
-	'0': 0b0000,
-	'1': 0b0001,
-	'2': 0b0010,
-	'3': 0b0011,
-	'4': 0b0100,
-	'5': 0b0101,
-	'6': 0b0110,
-	'7': 0b0111,
-	'8': 0b1000,
-	'9': 0b1001,
-	'A': 0b1010,
-	'B': 0b1011,
-	'C': 0b1100,
-	'D': 0b1101,
-	'E': 0b1110,
-	'F': 0b1111,
-}
-
-type PacketReader struct {
-	bits Bits
-}
-
-func NewPacketReader(asciiHex []byte) *PacketReader {
-	var bits Bits
-	for _, c := range asciiHex {
-		b := hexMapping[c]
-		for shift := 3; shift >= 0; shift-- {
-			if b&(1<<shift) != 0 {
-				bits = append(bits, 1)
+func SimulateX(dx int, area TargetArea) (hit bool) {
+	x := 0
+	for {
+		x += dx
+		dx -= SignInt(x)
+		if dx == 0 {
+			if area.ContainsX(x) {
+				return true
 			} else {
-				bits = append(bits, 0)
+				return false
 			}
 		}
 	}
-	return &PacketReader{bits: bits}
 }
 
-func (r *PacketReader) Read() Packet {
-	packet, _ := r.readPacket()
-	return packet
-}
-
-func (r *PacketReader) readPacket() (Packet, int) {
-	var initialLen = len(r.bits)
-
-	var packet Packet
-	packet.Version, _ = r.readVersion()
-	packet.TypeID, _ = r.readTypeID()
-
-	if packet.TypeID == 4 {
-		packet.LiteralValue, _ = r.readLiteralValue()
-	} else if packet.TypeID != 4 {
-		packet.SubPackets, _ = r.readOperator()
+func FindDy(area TargetArea) int {
+	var lastDy int
+	for dy := 0; dy <= -area.MinY; dy++ {
+		hit := SimulateY(dy, area)
+		if hit {
+			lastDy = dy
+		}
 	}
-
-	return packet, initialLen - len(r.bits)
+	return lastDy
 }
 
-func (r *PacketReader) readVersion() (int, int) {
-	var initialLen = len(r.bits)
-	version := r.bits[0:3].AsInt()
-	r.bits = r.bits[3:]
-	return version, initialLen - len(r.bits)
-}
-
-func (r *PacketReader) readTypeID() (int, int) {
-	var initialLen = len(r.bits)
-	typeID := r.bits[0:3].AsInt()
-	r.bits = r.bits[3:]
-	return typeID, initialLen - len(r.bits)
-}
-
-func (r *PacketReader) readLiteralValue() (int64, int) {
-	var initialLen = len(r.bits)
-
-	var literalBits Bits
+func SimulateY(dy int, area TargetArea) (hit bool) {
+	y := 0
 	for {
-		done := false
-		if r.bits[0] == 0 {
-			done = true
+		y += dy
+		dy--
+		if area.ContainsY(y) {
+			return true
 		}
-		literalBits = append(literalBits, r.bits[1:5]...)
-		r.bits = r.bits[5:]
-		if done {
-			break
+		if y < area.MinY {
+			return false
 		}
 	}
-	return literalBits.AsInt64(), initialLen - len(r.bits)
 }
 
-func (r *PacketReader) readOperator() ([]Packet, int) {
-	var initialLen = len(r.bits)
+func main() {
+	targetArea := TargetArea{
+		MinX: 137,
+		MaxX: 171,
+		MinY: -98,
+		MaxY: -73,
+	}
+	_ = targetArea
 
-	lengthTypeId := r.bits[0:1].AsInt()
-	r.bits = r.bits[1:]
+	//targetArea = TargetArea{
+	//	MinX: 20,
+	//	MaxX: 30,
+	//	MinY: -10,
+	//	MaxY: -5,
+	//}
 
-	var subPackets []Packet
-	if lengthTypeId == 0 {
-		remainingBitsToRead := r.bits[0:15].AsInt()
-		r.bits = r.bits[15:]
-		for remainingBitsToRead > 0 {
-			subMsg, bitsRead := r.readPacket()
-			subPackets = append(subPackets, subMsg)
-			remainingBitsToRead -= bitsRead
-		}
-	} else if lengthTypeId == 1 {
-		remainingSubPacketsToRead := r.bits[0:11].AsInt()
-		r.bits = r.bits[11:]
-		for remainingSubPacketsToRead > 0 {
-			subMsg, _ := r.readPacket()
-			subPackets = append(subPackets, subMsg)
-			remainingSubPacketsToRead--
+	// 3570 too low
+	// 3655 too low
+	// 4095 too low
+	// 4753 ?
+	// 4753 !!
+
+	dx1 := FindDx(targetArea)
+	dy1 := FindDy(targetArea)
+	fmt.Println("dy is", dy1)
+	fmt.Println("dx is", dx1)
+
+	for dx := dx1+2; dx <= dx1+3; dx++ {
+		for dy := dy1-3; dy <= dy1+3; dy++ {
+			fmt.Println(Simulate(dx, dy, targetArea))
 		}
 	}
 
-	return subPackets, initialLen - len(r.bits)
 }
