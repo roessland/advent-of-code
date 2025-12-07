e7475b554b03dc33848ed3f3dddefb588034a284
Andreas Røssland <andreas@enode.io>
2025-12-07 18:52:43 +0100

    2025 day 04 in go

---
+++ 2025/day-04/day04/day04.go
@@ -0,0 +1,95 @@
+// Package day04 solves AoC 2025 Day 4
+package day04
+
+import (
+	"embed"
+
+	"github.com/roessland/advent-of-code/2024/aocutil"
+)
+
+//go:embed input*.txt
+var InputFile embed.FS
+
+func ReadInput(inputName string) (grid [][]byte) {
+	return aocutil.ReadLinesAsBytes(inputName)
+}
+
+type Pos struct {
+	I, J int
+}
+
+func AdjacentPositions(grid [][]byte, p Pos) (ns []Pos) {
+	var cs []Pos
+	cs = append(cs, Pos{p.I - 1, p.J - 1})
+	cs = append(cs, Pos{p.I - 1, p.J - 0})
+	cs = append(cs, Pos{p.I - 1, p.J + 1})
+
+	cs = append(cs, Pos{p.I, p.J - 1})
+	cs = append(cs, Pos{p.I, p.J + 1})
+
+	cs = append(cs, Pos{p.I + 1, p.J - 1})
+	cs = append(cs, Pos{p.I + 1, p.J - 0})
+	cs = append(cs, Pos{p.I + 1, p.J + 1})
+	for _, c := range cs {
+		if c.I < 0 || c.I >= len(grid) {
+			continue
+		}
+		if c.J < 0 || c.J >= len(grid[0]) {
+			continue
+		}
+		ns = append(ns, c)
+	}
+	return ns
+}
+
+// NumRollsOfPaper counts the '@' in the given positions
+func NumRollsOfPaper(grid [][]byte, ps []Pos) (num int) {
+	for _, p := range ps {
+		if grid[p.I][p.J] == '@' {
+			num++
+		}
+	}
+	return num
+}
+
+// Accessible returns whether a position is paper and accessible by forklift
+func Accessible(grid [][]byte, p Pos) bool {
+	if grid[p.I][p.J] != '@' {
+		return false
+	}
+	adjacentPositions := AdjacentPositions(grid, p)
+	return NumRollsOfPaper(grid, adjacentPositions) < 4
+}
+
+func ForEachPosition(grid [][]byte, fn func(p Pos, c byte)) {
+	for i := range grid {
+		for j := range grid[i] {
+			fn(Pos{i, j}, grid[i][j])
+		}
+	}
+}
+
+// Part1 returns number of rolls of paper that can be accessed by a forklift
+func Part1(grid [][]byte) (accessible int) {
+	ForEachPosition(grid, func(p Pos, c byte) {
+		if Accessible(grid, p) {
+			accessible++
+		}
+	})
+	return accessible
+}
+
+// Part2 returns number of rolls of paper that can eventually accessed
+func Part2(grid [][]byte) (accessible int) {
+	for done := false; !done; {
+		done = true
+		ForEachPosition(grid, func(p Pos, c byte) {
+			if Accessible(grid, p) {
+				accessible++
+				grid[p.I][p.J] = '.'
+				done = false
+			}
+		})
+	}
+	return accessible
+}
---
+++ 2025/day-04/day04/day04_test.go
@@ -0,0 +1,18 @@
+package day04_test
+
+import (
+	"testing"
+
+	"github.com/roessland/advent-of-code/2025/day-04/day04"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadInput(t *testing.T) {
+	inputEx := day04.ReadInput("input-ex1.txt")
+	require.Equal(t, 13, day04.Part1(inputEx))
+	require.Equal(t, 43, day04.Part2(inputEx))
+
+	input := day04.ReadInput("input.txt")
+	require.Equal(t, 1523, day04.Part1(input))
+	require.Equal(t, 1523, day04.Part2(input))
+}
