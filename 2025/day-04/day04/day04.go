af6f2e00d47404d9350abb86f8533ec16002da87
Andreas Røssland <andreas@enode.io>
2025-12-07 19:21:54 +0100

    nicer code with iterators

--- 2025/day-04/day04/day04.go
+++ 2025/day-04/day04/day04.go
@@ -3,29 +3,35 @@ package day04
 
 import (
 	"embed"
+	"iter"
 
 	"github.com/roessland/advent-of-code/2024/aocutil"
 )
 
 //go:embed input*.txt
-var InputFile embed.FS
+var InputFS embed.FS
 
-func ReadInput(inputName string) (grid [][]byte) {
-	return aocutil.ReadLinesAsBytes(inputName)
+func ReadInput(inputName string) [][]byte {
+	return aocutil.FSReadLinesAsBytes(InputFS, inputName)
 }
 
 type Pos struct {
 	I, J int
 }
 
-func AdjacentPositions(grid [][]byte, p Pos) (ns []Pos) {
+func (p Pos) Valid(grid [][]byte) bool {
+	return p.I >= 0 && p.I < len(grid) && p.J >= 0 && p.J < len(grid[0])
+}
+
+func AdjacentPositions(grid [][]byte, p Pos) []Pos {
+	ns := make([]Pos, 0, 8)
 	for di := -1; di <= 1; di++ {
 		for dj := -1; dj <= 1; dj++ {
 			if di == 0 && dj == 0 {
 				continue
 			}
 			c := Pos{p.I + di, p.J + dj}
-			if c.I >= 0 && c.I < len(grid) && c.J >= 0 && c.J < len(grid[0]) {
+			if c.Valid(grid) {
 				ns = append(ns, c)
 			}
 		}
@@ -34,7 +40,8 @@ func AdjacentPositions(grid [][]byte, p Pos) (ns []Pos) {
 }
 
 // NumRollsOfPaper counts the '@' in the given positions
-func NumRollsOfPaper(grid [][]byte, ps []Pos) (num int) {
+func NumRollsOfPaper(grid [][]byte, ps []Pos) int {
+	num := 0
 	for _, p := range ps {
 		if grid[p.I][p.J] == '@' {
 			num++
@@ -48,39 +55,44 @@ func Accessible(grid [][]byte, p Pos) bool {
 	if grid[p.I][p.J] != '@' {
 		return false
 	}
-	adjacentPositions := AdjacentPositions(grid, p)
-	return NumRollsOfPaper(grid, adjacentPositions) < 4
+	return NumRollsOfPaper(grid, AdjacentPositions(grid, p)) < 4
 }
 
-func ForEachPosition(grid [][]byte, fn func(p Pos, c byte)) {
-	for i := range grid {
-		for j := range grid[i] {
-			fn(Pos{i, j}, grid[i][j])
+func Positions(grid [][]byte) iter.Seq[Pos] {
+	return func(yield func(Pos) bool) {
+		for i := range grid {
+			for j := range grid[i] {
+				if !yield(Pos{i, j}) {
+					return
+				}
+			}
 		}
 	}
 }
 
 // Part1 returns number of rolls of paper that can be accessed by a forklift
-func Part1(grid [][]byte) (accessible int) {
-	ForEachPosition(grid, func(p Pos, c byte) {
+func Part1(grid [][]byte) int {
+	accessible := 0
+	for p := range Positions(grid) {
 		if Accessible(grid, p) {
 			accessible++
 		}
-	})
+	}
 	return accessible
 }
 
 // Part2 returns number of rolls of paper that can eventually be accessed
-func Part2(grid [][]byte) (accessible int) {
+func Part2(grid [][]byte) int {
+	accessible := 0
 	for {
 		found := false
-		ForEachPosition(grid, func(p Pos, c byte) {
+		for p := range Positions(grid) {
 			if Accessible(grid, p) {
 				accessible++
 				grid[p.I][p.J] = '.'
 				found = true
 			}
-		})
+		}
 		if !found {
 			return accessible
 		}
